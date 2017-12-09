af1692f762c0228c65e85f89c3a98361b25012c2
Dito <dominik.dito@gmail.com>
2017-12-09 11:21:19 +0100

    Karcinom v69

--- gamePlay.py
+++ gamePlay.py
@@ -2,13 +2,17 @@ from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 from hero import Hero, Field
 from queue import Queue
+import time
 
 import json
+
+bfsCounterMax = 50
+bfsCounter = bfsCounterMax
 
 def parseHero (hero):
     id = hero['id'] #we know it is hero
     name = hero['name']
-    userId = hero['userId']
+    # userId = hero['userId']
     pos = (hero['pos']['x'], hero['pos']['y'])
     life = hero['life']
     gold = hero['gold']
@@ -16,7 +20,7 @@ def parseHero (hero):
     spawnPos = hero['spawnPos']
     chrased = hero['crashed']
 
-    return id, name, userId, pos, life, gold, mineCount, spawnPos, chrased
+    return id, name, pos, life, gold, mineCount, spawnPos, chrased
 
 
 def callDirectionAlgorithm (fun, goodBoard, heroMy, heroTheir):
@@ -46,30 +50,133 @@ def parseBoard(boardSize, boardTiles):
     rows = lenOfFields//boardSize
     finalBoard = []
 
-    print (lenOfFields, boardSize)
+    # print (lenOfFields, boardSize)
 
     currStart = 0
     for i in range(0, rows):
         finalBoard.append(fields[currStart: currStart + boardSize])
         currStart += boardSize
 
-
     return finalBoard
 
+def incPos(pos, direction):
+    # print (pos)
+    y, x= pos
+    if (direction == 'North'):
+        return (y-1, x)
+    if (direction == 'South'):
+        return (y+1, x)
+    if (direction == 'East'):
+        return (y, x+1)
+    if (direction == 'West'):
+        return (y, x-1)
+    raise Exception()
+
+###current request###
+
+def bfsToClosesX(goodBoard, heroMy, heroTheir, whereTo, ignoreds):
+    queue = Queue()
+    # queue
+    bfsCounter = bfsCounterMax
+
+    visited = set()
+    
+    for direction in ['North', 'South', 'East', 'West']:
+        nextPos = incPos(heroMy.pos, direction)
+        ny, nx = nextPos
+
+        if nx < 0 or ny < 0 or nx >= len(goodBoard) or ny >= len(goodBoard):
+            continue
+
+        currTile = goodBoard[ny][nx]
+        if (currTile in whereTo):
+            # printb (goodBoard)
+            # print (direction)
+            return (direction, 1)
+            
+        if currTile in ignoreds:
+            continue
+
+        queue.put((nextPos, [direction]))
+
+    while not queue.empty():
+        nexty, direction = queue.get()
+        # print (direction)
+        # print (0)
+        visited.add(nexty)
+
+        if bfsCounter <= 0:
+            bfsCounter = bfsCounterMax
+            if time.time() - start > 0.8:
+                return ('North', 999)
+        
+        bfsCounter -= 1
+
+        for dire in ['North', 'South', 'East', 'West']:
+            nextnext = incPos(nexty, dire)
+            if nextnext in visited:
+                continue
+                
+            ny, nx = nextnext
+
+            if nx < 0 or ny < 0 or nx >= len(goodBoard) or ny >= len(goodBoard):
+                continue
+
+            # print(nextnext)
+            currTile = goodBoard[ny][nx]
+            # print(currTile)
+            if currTile in whereTo:
+                # printb (goodBoard)
+                direction.append(dire)
+                # print (direction)
+                return (direction[0], len(direction))
+            
+            if currTile in ignoreds:
+                continue
+            
+            cloned = list(direction)
+            cloned.append(dire)
+            queue.put((nextnext, cloned))
+
+def countTiles (goodBoard, tileTypes):
+    return sum ([sum ([(1 if (tile in tileTypes) else 0) for tile in line]) for line in goodBoard])
+
+def getPathToHeal(goodBoard, heroMy, heroTheir):
+    return bfsToClosesX(goodBoard, heroMy, heroTheir, ['[]'], ['$2', '$-', '##', '$1', '@2'])[0]
+
+def getPathToGranny(goodBoard, heroMy, heroTheir):
+    return bfsToClosesX(goodBoard, heroMy, heroTheir, [heroTheir.heroTile], ['[]', '$2', '$-', '##', '$1'])[0]
+
+def getPathToMine(goodBoard, heroMy, heroTheir):
+    return bfsToClosesX(goodBoard, heroMy, heroTheir, [heroTheir.mineTile, '$-'], ['[]', '$-', '##', heroMy.mineTile])[0]
+
+def getDistanceToHeal(goodBoard, heroMy, heroTheir):
+    # print(heroTheir)
+    # print(heroMy)
+    return bfsToClosesX(goodBoard, heroMy, heroTheir, ['[]'], ['$2', '$-', '##', '$1', '@2'])[1]
+
+def getDistanceToGranny(goodBoard, heroMy, heroTheir):
+    return bfsToClosesX(goodBoard, heroMy, heroTheir, [heroTheir.heroTile], ['[]', '$2', '$-', '##', '$1'])[1]
+
+def getDistanceToMine(goodBoard, heroMy, heroTheir):
+    return bfsToClosesX(goodBoard, heroMy, heroTheir, [heroTheir.mineTile, '$-'], ['[]', '$-', '##', heroMy.mineTile])[1]
 
 ### init request ####
 
-post_fields = {'key': 'eyx3wvrg', 'turns':200}     # actually 6 turns, it returns 3*4 = 12
-url = 'http://192.168.3.251:9000/api/training'
+post_fields = {'key': 'h7hhobdd', 'turns':200}     # actually 6 turns, it returns 3*4 = 12
+url = 'http://192.168.2.104:9000/api/arena'
 request = Request(url, urlencode(post_fields).encode())
 jsonData = json.loads(urlopen(request).read().decode())
 
 ### init request end ###
 
 bol = False
+start = time.time()
 
 while True:
-
+    old = start
+    start = time.time()
+    print (start-old)
     # at beggining fetched before while, will fetch at the end of loop
 
     game = jsonData['game']
@@ -86,25 +193,27 @@ while True:
         break
 
     hero = jsonData['hero']
-    id, name, userId, pos, life, gold, mineCount, spawnPos, crashed = parseHero(hero)
-    heroMy = Hero (id, name, userId, pos, life, gold, mineCount, spawnPos, crashed)
+    id, name, pos, life, gold, mineCount, spawnPos, crashed = parseHero(hero)
+    heroMy = Hero (id, name, pos, life, gold, mineCount, spawnPos, crashed)
     heroTheir = None
 
     # iscitam ovog, to sam ja, i usporedim, na kraju imam njega i enemy heroa
 
     for concreteHero in heroes:
-        id, name, userId, pos, life, gold, mineCount, spawnPos, crashed = parseHero(hero)
+        # print(concreteHero)
+        id, name, pos, life, gold, mineCount, spawnPos, crashed = parseHero(concreteHero)
 
         if id == heroMy.id:
             continue
         else:
-            heroTheir = Hero (id, name, userId, pos, life, gold, mineCount, spawnPos, crashed)
+            # print (concreteHero)
+            heroTheir = Hero (id, name, pos, life, gold, mineCount, spawnPos, crashed)
 
 
     goodBoard = parseBoard(boardSize, boardTiles)
 
-    print ("numRows {}, numCols {}".format(len(goodBoard), len(goodBoard[0]) ))
-    print ("turn {}, maxturns {}".format(turn, maxTurns))
+    # print ("numRows {}, numCols {}".format(len(goodBoard), len(goodBoard[0]) ))
+    # print ("turn {}, maxturns {}".format(turn, maxTurns))
 
 
     # for now not using, but will parse when decide which one will be which
@@ -113,116 +222,43 @@ while True:
     viewUrl = jsonData['viewUrl']
     playUrl = jsonData['playUrl'] # koristi se za zahtjev
 
-
     print ("viewUrl {}".format(viewUrl))
-
-    def incPos(pos, direction):
-        # print (pos)
-        y, x= pos
-        if (direction == 'North'):
-            return (y-1, x)
-        if (direction == 'South'):
-            return (y+1, x)
-        if (direction == 'East'):
-            return (y, x+1)
-        if (direction == 'West'):
-            return (y, x-1)
-        raise Exception()
-
-    ###current request###
-
-    def bfsToClosesX(goodBoard, heroMy, heroTheir, whereTo, ignoreds):
-        queue = Queue()
-        # queue
-
-        visited = set()
-        
-        for direction in ['North', 'South', 'East', 'West']:
-            nextPos = incPos(heroMy.pos, direction)
-            ny, nx = nextPos
-
-            if nx < 0 or ny < 0 or nx >= len(goodBoard) or ny > len(goodBoard):
-                continue
-            
-            currTile = goodBoard[ny][nx]
-            if (currTile in whereTo):
-                printb (goodBoard)
-                print (direction)
-                return direction
-                
-            if currTile in ignoreds:
-                continue
-
-            queue.put((nextPos, [direction]))
-            visited.add(nextPos)
-
-        while not queue.empty():
-            nexty, direction = queue.get()
-            # print (direction)
-            # print (0)
-            visited.add(nexty)
-
-            for dire in ['North', 'South', 'East', 'West']:
-                nextnext = incPos(nexty, dire)
-                if nextnext in visited:
-                    continue
-                    
-                ny, nx = nextnext
-
-                if nx < 0 or ny < 0 or nx >= len(goodBoard) or ny >= len(goodBoard):
-                    continue
-
-                # print(nextnext)
-                currTile = goodBoard[ny][nx]
-                # print(currTile)
-                if currTile in whereTo:
-                    printb (goodBoard)
-                    direction.append(dire)
-                    print (direction)
-                    return direction[0]
-                
-                if currTile in ignoreds:
-                    continue
-                
-                cloned = list(direction)
-                cloned.append(dire)
-                queue.put((nextnext, cloned))
-
-    def countTiles (goodBoard, tileTypes):
-        return sum ([sum ([(1 if (tile in tileTypes) else 0) for tile in line]) for line in goodBoard])
 
     def algorithm2 (goodBoard, heroMy, heroTheir):
         # print (str(heroMy.pos['x']) + " " + str(heroMy.pos['y']))
-        print (heroMy.life)
-        if heroMy.life < 30:
-            return bfsToClosesX(goodBoard, heroMy, heroTheir, ['[]'], ['$2', '$-', '##', '$1'])
+        # print (heroMy.life)
+        if heroMy.life < 40:
+            return getPathToHeal(goodBoard, heroMy, heroTheir)
 
-        if countTiles(goodBoard, ['$-', '$2']) <= 1:
+        if heroTheir.mineCount - heroMy.mineCount > 4:
+            if heroMy.life + 10 < heroTheir.life:
+                return getPathToHeal(goodBoard, heroMy, heroTheir)
+            return getPathToGranny(goodBoard, heroMy, heroTheir)
+
+        if getDistanceToGranny(goodBoard, heroMy, heroTheir) < 5:
+            # print (heroTheir)
+            if heroMy.life < heroTheir.life:
+                return getPathToHeal(goodBoard, heroMy, heroTheir)
+
+        if countTiles(goodBoard, ['$-', heroTheir.mineTile]) < 3:
             if heroMy.life < 60:
-                bfsToClosesX(goodBoard, heroMy, heroTheir, ['[]'], ['$2', '$-', '##', '$1'])
+                return getPathToHeal(goodBoard, heroMy, heroTheir)
             else:
-                bfsToClosesX(goodBoard, heroMy, heroTheir, ['@2'], ['[]', '$2', '$-', '##', '$1'])
+                return getPathToGranny(goodBoard, heroMy, heroTheir)
 
-        return bfsToClosesX(goodBoard, heroMy, heroTheir, ['$2', '$-'], ['##', '$1', '[]'])
+        return getPathToMine(goodBoard, heroMy, heroTheir)
 
 
 
     ##complicated algo to determine direction###
-
+    # print (heroTheir)
     dir = callDirectionAlgorithm (algorithm2, goodBoard, heroMy, heroTheir)
 
     ############################################
 
     # Stay, North, South, East, West
 
-    # if bol:
-    #     dir = 'North'
-    #     bol = not bol
-    # else:
-    #     dir = 'South'
-    #     bol = not bol
-
-    post_fields = {'key': 'eyx3wvrg', 'dir': dir}
+    post_fields = {'key': 'h7hhobdd', 'dir': dir}
 
     url = playUrl  # Set destination URL here
     request = Request(url, urlencode(post_fields).encode())
--- hero.py
+++ hero.py
@@ -3,16 +3,18 @@ class Hero:
 
     # pos, spawnPos su dictovi
 
-    def __init__(self, id, name, userId, pos, life, gold, mineCount, spawnPos, crashed):
+    def __init__(self, id, name, pos, life, gold, mineCount, spawnPos, crashed):
         self.id = id
         self.name = name
-        self.userId = userId
+        # self.userId = userId
         self.pos = pos # dict sa x, y
         self.life = life
         self.gold = gold
         self.mineCount = mineCount
         self.spawnPos = spawnPos # dict sa x, y
         self.crashed = crashed
+        self.heroTile = "@1" if id == 1 else "@2";
+        self.mineTile = "$1" if id == 1 else "$2";
 
 
 class Field:
