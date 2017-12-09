335a8a53e681fe52a5f73983b7891e76096c73ca
Dito <dominik.dito@gmail.com>
2017-12-09 06:05:29 +0100

    Branching off zmaj

--- gamePlay.py
+++ gamePlay.py
@@ -1,6 +1,7 @@
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 from hero import Hero, Field
+from queue import Queue
 
 import json
 
@@ -8,7 +9,7 @@ def parseHero (hero):
     id = hero['id'] #we know it is hero
     name = hero['name']
     userId = hero['userId']
-    pos = hero['pos']
+    pos = (hero['pos']['x'], hero['pos']['y'])
     life = hero['life']
     gold = hero['gold']
     mineCount = hero['mineCount']
@@ -19,11 +20,13 @@ def parseHero (hero):
 
 
 def callDirectionAlgorithm (fun, goodBoard, heroMy, heroTheir):
-    fun (goodBoard, heroMy, heroTheir)
+    return fun (goodBoard, heroMy, heroTheir)
 
 def algorithm1 (goodBoard, heroMy, heroTheir):
     return None
 
+def printb(board):
+    [print("".join(b)) for b in board]
 
 def parseBoard(boardSize, boardTiles):
 
@@ -36,24 +39,7 @@ def parseBoard(boardSize, boardTiles):
     stateBefore = 0
 
     while (len(boardTiles) > 0):
-
-        if boardTiles[0:2] == '  ':
-            fields.append(Field('Prazno'))
-        if boardTiles[0:2] == '##':
-            fields.append(Field('Prepreka'))
-        elif boardTiles[0:2] == '@1':
-            fields.append(Field('Heroj1'))
-        elif boardTiles[0:2] == '@2':
-            fields.append(Field('Heroj2'))
-        elif boardTiles[0:2] == '[]':
-            fields.append('AparatZaKavu')
-        elif boardTiles[0:2] == '$-':
-            fields.append('RudnikNeutralan')
-        elif boardTiles[0:2] == '$1':
-            fields.append('Rudnik1')
-        elif boardTiles[0:2] == '$2':
-            fields.append('Rudnik2')
-
+        fields.append(boardTiles[0:2])
         boardTiles = boardTiles[2:]
 
     lenOfFields = len(fields)
@@ -73,7 +59,7 @@ def parseBoard(boardSize, boardTiles):
 
 ### init request ####
 
-post_fields = {'key': 'eyx3wvrg', 'turns':2}     # actually 6 turns, it returns 3*4 = 12
+post_fields = {'key': 'eyx3wvrg', 'turns':200}     # actually 6 turns, it returns 3*4 = 12
 url = 'http://192.168.3.251:9000/api/training'
 request = Request(url, urlencode(post_fields).encode())
 jsonData = json.loads(urlopen(request).read().decode())
@@ -130,27 +116,111 @@ while True:
 
     print ("viewUrl {}".format(viewUrl))
 
-
+    def incPos(pos, direction):
+        # print (pos)
+        y, x= pos
+        if (direction == 'North'):
+            return (y-1, x)
+        if (direction == 'South'):
+            return (y+1, x)
+        if (direction == 'East'):
+            return (y, x+1)
+        if (direction == 'West'):
+            return (y, x-1)
+        raise Exception()
 
     ###current request###
 
+    def bfsToClosesX(goodBoard, heroMy, heroTheir, whereTo, ignoreds):
+        queue = Queue()
+        # queue
+
+        visited = set()
+        
+        for direction in ['North', 'South', 'East', 'West']:
+            nextPos = incPos(heroMy.pos, direction)
+            ny, nx = nextPos
+
+            if nx < 0 or ny < 0 or nx >= len(goodBoard) or ny > len(goodBoard):
+                continue
+            
+            currTile = goodBoard[ny][nx]
+            if (currTile in whereTo):
+                printb (goodBoard)
+                print (direction)
+                return direction
+                
+            if currTile in ignoreds:
+                continue
+
+            queue.put((nextPos, [direction]))
+            visited.add(nextPos)
+
+        while not queue.empty():
+            nexty, direction = queue.get()
+            # print (direction)
+            # print (0)
+            visited.add(nexty)
+
+            for dire in ['North', 'South', 'East', 'West']:
+                nextnext = incPos(nexty, dire)
+                if nextnext in visited:
+                    continue
+                    
+                ny, nx = nextnext
+
+                if nx < 0 or ny < 0 or nx >= len(goodBoard) or ny >= len(goodBoard):
+                    continue
+
+                # print(nextnext)
+                currTile = goodBoard[ny][nx]
+                # print(currTile)
+                if currTile in whereTo:
+                    printb (goodBoard)
+                    direction.append(dire)
+                    print (direction)
+                    return direction[0]
+                
+                if currTile in ignoreds:
+                    continue
+                
+                cloned = list(direction)
+                cloned.append(dire)
+                queue.put((nextnext, cloned))
+
+    def countTiles (goodBoard, tileTypes):
+        return sum ([sum ([(1 if (tile in tileTypes) else 0) for tile in line]) for line in goodBoard])
+
+    def algorithm2 (goodBoard, heroMy, heroTheir):
+        # print (str(heroMy.pos['x']) + " " + str(heroMy.pos['y']))
+        print (heroMy.life)
+        if heroMy.life < 30:
+            return bfsToClosesX(goodBoard, heroMy, heroTheir, ['[]'], ['$2', '$-', '##', '$1'])
+
+        if countTiles(goodBoard, ['$-', '$2']) <= 1:
+            if heroMy.life < 60:
+                bfsToClosesX(goodBoard, heroMy, heroTheir, ['[]'], ['$2', '$-', '##', '$1'])
+            else:
+                bfsToClosesX(goodBoard, heroMy, heroTheir, ['@2'], ['[]', '$2', '$-', '##', '$1'])
+
+        return bfsToClosesX(goodBoard, heroMy, heroTheir, ['$2', '$-'], ['##', '$1', '[]'])
 
 
 
     ##complicated algo to determine direction###
 
-    dir = callDirectionAlgorithm (algorithm1, goodBoard, heroMy, heroTheir)
+    dir = callDirectionAlgorithm (algorithm2, goodBoard, heroMy, heroTheir)
 
     ############################################
 
     # Stay, North, South, East, West
 
-    if bol:
-        dir = 'North'
-        bol = not bol
-    else:
-        dir = 'South'
-        bol = not bol
+    # if bol:
+    #     dir = 'North'
+    #     bol = not bol
+    # else:
+    #     dir = 'South'
+    #     bol = not bol
 
     post_fields = {'key': 'eyx3wvrg', 'dir': dir}
 
@@ -159,9 +229,6 @@ while True:
     jsonData = json.loads(urlopen(request).read().decode())
 
     ####current request end ###
-
-
-
 
 
 
